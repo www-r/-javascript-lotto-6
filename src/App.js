0bef5580141823f04df6b95a7ced75bf6c1a4e0a
김영은 <jessicak0828@gmail.com>
2023-11-07 22:36:20 +0900

    feat: App에서 순서대로 기능 함수들 구성하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import View from './View';
+import Computer from './Computer';
+import { RESPONSE } from './constants';
+
 class App {
-  async play() {}
+	async play() {
+		const userMoney = await View.getUserMoney();
+		View.print(userMoney);
+		const userLottosAmount = Computer.calculateAmount(userMoney);
+		View.print(`${RESPONSE.AMOUNT_IS(userLottosAmount)}`);
+		const userLottosArr = Computer.createTotalLottosArr(userLottosAmount);
+		const numbers = await View.getNumbers();
+		View.print(...numbers);
+		const userBonus = await View.getUserBonus();
+		View.print(userBonus);
+		const result = Computer.calculateResult(userLottosArr, numbers, userBonus);
+		const profitsRate = Computer.calculateProfitsRate(result, userMoney);
+		View.printStatisticsResult(result);
+		View.print(RESPONSE.PROFITS_RATE_IS(profitsRate));
+	}
 }
 
 export default App;
